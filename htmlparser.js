6f4e715c0ac3928718bbc219fd4345f71911099d
Kevin Burke <kevin@twilio.com>
2013-02-10 20:15:17 -0800

    parse errors into an error list

--- htmlparser.js
+++ htmlparser.js
@@ -1,7 +1,7 @@
 $(function() {
 
-    var errorDiv = 'htmlparser-errors';
-    var $error = '#' + errorDiv;
+    var errorDivName = 'htmlparser-errors';
+    var $error = '#' + errorDivName;
 
     var getPageSource = function(callback) {
         return $.get('', function(data) {
@@ -9,10 +9,12 @@ $(function() {
         });
     };
 
-    var getErrorDiv = function(errorString, err) {
-        return $("<div id='" + errorDiv + "'>" + errorString + ": " + err +
-            "</div>"
-            ).css({
+    var insertErrorDiv = function(errorString, err) {
+        var list = $("<ul id='htmlparser-errors-list'></ul>").css({
+            padding: 0,
+            margin: 0
+        });
+        var $div = $("<div id='" + errorDivName + "'>" + "</div>").css({
                 backgroundColor: '#f2dede',
                 padding: '10px 25px',
                 border: '1px solid #eed3d7',
@@ -23,21 +25,43 @@ $(function() {
                 marginBottom: '7px',
             }
         );
+        $div.append(list);
+        $('body').prepend($div);
     };
 
-    var showError = function(errorString, err, index, html) {
-        if ($($error).length === 0) {
-            $('body').prepend(getErrorDiv(errorString, err));
+    var showErrors = function(errorsList) {
+        if (errorsList.length === 0) {
+            return;
         }
+        insertErrorDiv();
+        for (var i = 0; i < errorsList.length; i++) {
+            appendError(errorsList[i]);
+        }
+    };
+
+    var appendError = function(error) {
+        $("#htmlparser-errors-list").append($("<li>" + error.errorString + ': ' + error.error + ' on line ' + error.line + "</li>"));
+    };
+
+    var getErrorLines = function(idx, html) {
+        // XXX
+        return idx;
     };
 
     var missingTrailingAttributeQuote = /(\S+=['"][^'" ]+) /g;
     var missingOpeningAttributeQuote = /(\S+=[^'"]+['"])[ >]/g;
     var evaluateHtml = function(html) {
         var result;
+        var errorsList = [];
         while ((result = missingOpeningAttributeQuote.exec(html)) !== null) {
-            showError("Missing quote after equals sign for attribute", result[1], result[2], html);
+            errorsList.push({
+                'errorString': "Missing quote after equals sign for attribute",
+                'error': result[1],
+                'line': getErrorLines(result.index, html),
+                'html': html
+            });
         }
+        showErrors(errorsList);
     };
 
     getPageSource(evaluateHtml);
