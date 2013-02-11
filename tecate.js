97906ac9ec180f7bcc394e5d462a74b692a22730
Kevin Burke <kevin@twilio.com>
2013-02-10 23:26:10 -0800

    cover both quote cases.

--- tecate.js
+++ tecate.js
@@ -1,27 +1,27 @@
 var htmlparser = htmlparser || {};
 
-var errors = [{
+htmlparser.errors = [{
     'regex': new RegExp("(\\S+=[^'\"]+['\"])[ >]", "g"),
     'message': "Missing quote after equals sign for attribute"
 }, {
-    'regex': new RegExp("(\\S+=['\"][^'\" ]+)[ >]", "g"),
+    'regex': new RegExp("(\\S+=['\"][^'\" >]+)[ >]", "g"),
     'message': "Missing quote at the end of the attribute"
 }];
 
-var errorDivName = 'htmlparser-errors';
+htmlparser.errorDivName = 'htmlparser-errors';
 
-var getPageSource = function(callback) {
+htmlparser.getPageSource = function(callback) {
     return $.get('', function(data) {
         callback(data);
     });
 };
 
-var insertErrorDiv = function() {
+htmlparser.insertErrorDiv = function() {
     var list = $("<ul id='htmlparser-errors-list'></ul>").css({
         padding: 0,
         margin: 0
     });
-    var $div = $("<div id='" + errorDivName + "'>" + "</div>").css({
+    var $div = $("<div id='" + htmlparser.errorDivName + "'>" + "</div>").css({
             backgroundColor: '#f2dede',
             padding: '10px 25px',
             border: '1px solid #eed3d7',
@@ -36,43 +36,42 @@ var insertErrorDiv = function() {
     $('body').prepend($div);
 };
 
-var appendError = function(error) {
-    $("#htmlparser-errors-list").append($("<li>" + error.errorString + ': ' + error.error + ' on line ' + error.line + "</li>"));
+htmlparser.appendError = function(error) {
+    $("#htmlparser-errors-list").append($("<li>" + error.errorString + ': <code>' + error.error + '</code> on line ' + error.line + "</li>"));
 };
 
-var showErrors = function(errorsList) {
+htmlparser.showErrors = function(errorsList) {
     if (errorsList.length === 0) {
         return;
     }
-    insertErrorDiv();
+    htmlparser.insertErrorDiv();
     for (var i = 0; i < errorsList.length; i++) {
-        appendError(errorsList[i]);
+        htmlparser.appendError(errorsList[i]);
     }
 };
 
-var getErrorLines = function(idx) {
+htmlparser.getErrorLines = function(idx) {
     // XXX
     return idx;
 };
 
-var evaluateHtml = function(html) {
+htmlparser.evaluateHtml = function(html) {
     var result;
     var errorsList = [];
-    for (var i = 0; i < errors.length; i++) {
-        var error = errors[i];
+    for (var i = 0; i < htmlparser.errors.length; i++) {
+        var error = htmlparser.errors[i];
         while ((result = error.regex.exec(html)) !== null) {
             errorsList.push({
                 'errorString': error.message,
                 'error': result[1],
-                'line': getErrorLines(result.index, html),
+                'line': htmlparser.getErrorLines(result.index, html),
                 'html': html
             });
         }
     }
-    showErrors(errorsList);
+    htmlparser.showErrors(errorsList);
 };
 
 $(function() {
-    getPageSource(evaluateHtml);
-
+    htmlparser.getPageSource(htmlparser.evaluateHtml);
 });
